788e9d8eacb6942c1139c88ffefa6c10f97deb41
꧁༒⟁₱Ⱨ₳₦₮Ø₼ Ø₣ ₮ⱧɆ VØłĐ⟁༒꧂ <harshthakur750556@gmail.com>
2026-09-02 20:44:47 +0530

    chore: clean temporary build scripts

--- inspect_path0.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const svg = fs.readFileSync('C:/Users/harsh/Downloads/Untitled.svg', 'utf8');
-
-const p0 = (svg.match(/<path d="([^"]+)"/) || [null, ''])[1];
-console.log("Path 0 start:", p0.slice(0, 100));
